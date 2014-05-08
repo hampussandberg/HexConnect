bc6c83d1960260c708ca4402231be09026bdefc9
Hampus Sandberg <hampus.sandberg@gmail.com>
2014-05-08 20:25:53 +0200

    First Commit

---
+++ Software/freertos-serial-monitor/src/main.c
@@ -0,0 +1,189 @@
+/*
+ * FreeRTOS Coding Standard and Style Guide:
+ * http://www.freertos.org/FreeRTOS-Coding-Standard-and-Style-Guide.html
+ */
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* STM32 Library includes. */
+#include "stm32f4xx_hal.h"
+
+#include <stdio.h>
+
+/* Priorities at which the tasks are created. */
+#define mainBLINK_TASK_PRIORITY				(tskIDLE_PRIORITY)
+
+/* ----- LED definitions --------------------------------------------------- */
+/* LEDs on STM32F4 Discovery Board */
+#define mainLED_3	GPIO_PIN_13
+#define mainLED_4	GPIO_PIN_12
+#define mainLED_5	GPIO_PIN_14
+#define mainLED_6	GPIO_PIN_15
+
+
+/* ----- Task definitions -------------------------------------------------- */
+static void prvBlinkTask(void *pvParameters);
+
+/* ----- Main -------------------------------------------------------------- */
+int main(int argc, char* argv[])
+{
+	/*
+	 * The NVIC priority group is set to NVIC_PRIORITYGROUP_4 in HAL_Init
+	 * called in _initialize_hardware.c
+	 * At this point everything is ready to go!
+	 */
+
+	/* Create the tasks */
+	xTaskCreate(prvBlinkTask,					/* Pointer to the task entry function */
+				"Blink",						/* Name for the task */
+				configMINIMAL_STACK_SIZE,		/* The size of the stack */
+				NULL,							/* Pointer to parameters for the task */
+				mainBLINK_TASK_PRIORITY,		/* The priority for the task */
+				NULL);							/* Handle for the created task */
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* If all is well, the scheduler will now be running, and the following line
+	will never be reached.  If the following line does execute, then there was
+	insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+	to be created.  See the memory management section on the FreeRTOS web site
+	for more details. */
+	while (1);
+}
+
+/*-----------------------------------------------------------*/
+static void prvBlinkTask(void *pvParameters)
+{
+	/* Set up the LED outputs */
+	// Enable clock for GPIOD
+	__GPIOD_CLK_ENABLE();
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.Pin  	= mainLED_3 | mainLED_4 | mainLED_5 | mainLED_6;
+	GPIO_InitStructure.Mode  	= GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStructure.Pull		= GPIO_NOPULL;
+	GPIO_InitStructure.Speed 	= GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	HAL_GPIO_WritePin(GPIOD, mainLED_3, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, mainLED_4, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, mainLED_5, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD, mainLED_6, GPIO_PIN_RESET);
+
+	/* The parameter in vTaskDelayUntil is the absolute time
+	 * in ticks at which you want to be woken calculated as
+	 * an increment from the time you were last woken. */
+	TickType_t xNextWakeTime;
+	/* Initialize xNextWakeTime - this only needs to be done once. */
+	xNextWakeTime = xTaskGetTickCount();
+
+	while (1)
+	{
+		/* LED on for 25 ms */
+		HAL_GPIO_WritePin(GPIOD, mainLED_6, GPIO_PIN_SET);
+		vTaskDelayUntil(&xNextWakeTime, 25 / portTICK_PERIOD_MS);
+
+		/* LED off for 1000 ms */
+		HAL_GPIO_WritePin(GPIOD, mainLED_6, GPIO_PIN_RESET);
+		vTaskDelayUntil(&xNextWakeTime, 1000 / portTICK_PERIOD_MS);
+	}
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook(void)
+{
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+	while (1);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName)
+{
+	(void) pcTaskName;
+	(void) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configconfigCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	while (1);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook(void)
+{
+	volatile size_t xFreeStackSpace;
+
+	/* This function is called on each cycle of the idle task.  In this case it
+	does nothing useful, other than report the amount of FreeRTOS heap that
+	remains unallocated. */
+	xFreeStackSpace = xPortGetFreeHeapSize();
+
+	if (xFreeStackSpace > 100)
+	{
+		/* By now, the kernel has allocated everything it is going to, so
+		if there is a lot of heap remaining unallocated then
+		the value of configTOTAL_HEAP_SIZE in FreeRTOSConfig.h can be
+		reduced accordingly. */
+	}
+}
+
+#ifdef DEBUG
+/* The fault handler implementation calls a function called
+prvGetRegistersFromStack(). */
+void HardFault_Handler(void)
+{
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word prvGetRegistersFromStack    \n"
+    );
+}
+
+void prvGetRegistersFromStack(uint32_t *pulFaultStackAddress)
+{
+	/* These are volatile to try and prevent the compiler/linker optimising them
+	away as the variables never actually get used.  If the debugger won't show the
+	values of the variables, make them global my moving their declaration outside
+	of this function. */
+	volatile uint32_t r0;
+	volatile uint32_t r1;
+	volatile uint32_t r2;
+	volatile uint32_t r3;
+	volatile uint32_t r12;
+	volatile uint32_t lr; /* Link register. */
+	volatile uint32_t pc; /* Program counter. */
+	volatile uint32_t psr;/* Program status register. */
+
+    r0 = pulFaultStackAddress[0];
+    r1 = pulFaultStackAddress[1];
+    r2 = pulFaultStackAddress[2];
+    r3 = pulFaultStackAddress[3];
+
+    r12 = pulFaultStackAddress[4];
+    lr = pulFaultStackAddress[5];
+    pc = pulFaultStackAddress[6];
+    psr = pulFaultStackAddress[7];
+
+    /* When the following line is hit, the variables contain the register values. */
+    while (1);
+
+    /* These lines help prevent getting warnings from compiler about unused variables */
+    r0 = r1 = r2 = r3 = r12 = lr = pc = psr = 0;
+    r0++;
+}
+
+#endif // #ifdef DEBUG
