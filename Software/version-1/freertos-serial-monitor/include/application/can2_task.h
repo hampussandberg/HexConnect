3eb7b4ec97da79cc581e81c9b816aa5ce8edba2c
Hampus Sandberg <hampus.sandberg@gmail.com>
2015-08-15 20:35:15 +0200

    Cleanup and updated licensing

--- Software/version-1/freertos-serial-monitor/include/application/can2_task.h
+++ Software/version-1/freertos-serial-monitor/include/application/can2_task.h
@@ -1,26 +1,26 @@
 /**
- ******************************************************************************
- * @file	can2_task.h
- * @author	Hampus Sandberg
- * @version	0.1
- * @date	2014-09-06
+ *******************************************************************************
+ * @file  can2_task.h
+ * @author  Hampus Sandberg
+ * @version 0.1
+ * @date  2015-08-15
  * @brief
- ******************************************************************************
-	Copyright (c) 2014 Hampus Sandberg.
+ *******************************************************************************
+  Copyright (c) 2015 Hampus Sandberg.
 
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation, either
-	version 3 of the License, or (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
 
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Lesser General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public
-	License along with this library; if not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
