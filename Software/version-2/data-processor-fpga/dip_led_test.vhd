1fe76fe2689415a480e689744af63361d9cd7ab3
Hampus Sandberg <hampus.sandberg@gmail.com>
2015-08-16 18:03:00 +0200

    Added README and license

--- Software/version-2/data-processor-fpga/dip_led_test.vhd
+++ Software/version-2/data-processor-fpga/dip_led_test.vhd
@@ -1,3 +1,26 @@
+-- *******************************************************************************
+-- * @file    dip_led_test.vhd
+-- * @author  Hampus Sandberg
+-- * @version 0.1
+-- * @date    2015-08-16
+-- * @brief
+-- *******************************************************************************
+--  Copyright (c) 2015 Hampus Sandberg.
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- *******************************************************************************
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
